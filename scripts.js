6a18cc0a10440c748ce62a15ea5f877a5a6110e5
Jan Makur <87923447+JanMakur@users.noreply.github.com>
2024-06-13 08:53:41 +0530

    commited

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.add('active'); // Ensure the nav menu is always displayed
+});
